3c1ed3063eaba7312a62b1c48c69f9524aa9316f
BTWitsK <BTWitsK@Gmail.com>
2022-03-20 19:37:38 -0600

    implemented stage 1

--- Simple Search Engine/task/src/search/Main.java
+++ Simple Search Engine/task/src/search/Main.java
@@ -1,7 +1,24 @@
 package search;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        String[] words = scanner.nextLine().split(" ");
+        String searchFor = scanner.nextLine();
+        boolean found = false;
+        int index = 0;
+
+        for (int i = 0; i < words.length; i++) {
+            if (searchFor.equals(words[i])) {
+                index = i;
+                found = true;
+                break;
+            }
+        }
+
+        String result = found ? String.valueOf(index + 1) : "Not found";
+        System.out.println(result);
+
     }
 }
