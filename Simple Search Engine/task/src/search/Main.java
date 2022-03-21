912b74dabb9b0be715c6dbd34d56015e5359a9a7
BTWitsK <BTWitsK@Gmail.com>
2022-03-21 00:43:25 -0600

    split main class into methods for clarity completed stage 2

--- Simple Search Engine/task/src/search/Main.java
+++ Simple Search Engine/task/src/search/Main.java
@@ -2,10 +2,39 @@ package search;
 import java.util.*;
 
 public class Main {
+    static ArrayList<String> peopleList = new ArrayList<>();
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void loadList(int inputLines) {
+        System.out.println("Enter all people:");
+        for (int i = 0; i < inputLines; i++) {
+            peopleList.add(scanner.nextLine());
+        }
+    }
+
+    public static void executeSearch(int numberOfQueries) {
+        ArrayList<String> tempList = new ArrayList<>();
+        for (int i = 0; i < numberOfQueries; i++) {
+            System.out.println("Enter data to search people:");
+            String searchField = scanner.nextLine().toLowerCase();
+
+            peopleList.forEach(line -> { if (line.toLowerCase().contains(searchField)) tempList.add(line); });
+
+            if (!tempList.isEmpty()) {
+                System.out.println("Found people:");
+                tempList.forEach(System.out::println);
+                tempList.clear();
+            } else {
+                System.out.println("No matching people found.");
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        List<String> words = List.of(scanner.nextLine().split(" "));
-        String searchFor = scanner.nextLine();
-        System.out.println(words.contains(searchFor) ? String.valueOf(words.indexOf(searchFor) + 1) : "Not found");
+        System.out.println("Enter the number of people:");
+        loadList(Integer.parseInt(scanner.nextLine()));
+
+        System.out.println("Enter the number of search queries:");
+        executeSearch(Integer.parseInt(scanner.nextLine()));
     }
 }
