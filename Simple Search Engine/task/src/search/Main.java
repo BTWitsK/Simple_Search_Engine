453310a7108aa28bd7bac2340b4bb76240a12d16
BTWitsK <BTWitsK@Gmail.com>
2022-03-21 19:05:37 -0600

    added enum Menu, main Menu switch completed stage 3

--- Simple Search Engine/task/src/search/Main.java
+++ Simple Search Engine/task/src/search/Main.java
@@ -2,39 +2,76 @@ package search;
 import java.util.*;
 
 public class Main {
+    enum Menu {
+        SEARCH(1),
+        PRINT(2),
+        EXIT(0);
+        final int choice;
+        Menu(int input) {
+            this.choice = input;
+        }
+    }
     static ArrayList<String> peopleList = new ArrayList<>();
     static Scanner scanner = new Scanner(System.in);
+    static Menu menu;
+
+    public static void setMenu() {
+        System.out.print("""
+                === Menu ===
+                1. Find a person
+                2. Print all people
+                0. Exit
+                """);
+
+        int choice = Integer.parseInt(scanner.nextLine());
+        for (Menu option : Menu.values()) {
+            if (option.choice == choice) {
+                menu = option;
+            }
+        }
+    }
 
     public static void loadList(int inputLines) {
+        System.out.println();
         System.out.println("Enter all people:");
         for (int i = 0; i < inputLines; i++) {
             peopleList.add(scanner.nextLine());
         }
     }
 
-    public static void executeSearch(int numberOfQueries) {
+    public static void executeSearch() {
         ArrayList<String> tempList = new ArrayList<>();
-        for (int i = 0; i < numberOfQueries; i++) {
-            System.out.println("Enter data to search people:");
-            String searchField = scanner.nextLine().toLowerCase();
+        System.out.println("Enter a name or email to search all suitable people.");
+        String searchField = scanner.nextLine().toLowerCase();
 
-            peopleList.forEach(line -> { if (line.toLowerCase().contains(searchField)) tempList.add(line); });
+        peopleList.forEach(line -> { if (line.toLowerCase().contains(searchField)) tempList.add(line); });
 
-            if (!tempList.isEmpty()) {
-                System.out.println("Found people:");
-                tempList.forEach(System.out::println);
-                tempList.clear();
-            } else {
-                System.out.println("No matching people found.");
-            }
+        if (!tempList.isEmpty()) {
+            tempList.forEach(System.out::println);
+        } else {
+            System.out.println("No matching people found.");
         }
     }
 
     public static void main(String[] args) {
         System.out.println("Enter the number of people:");
         loadList(Integer.parseInt(scanner.nextLine()));
+        System.out.println();
 
-        System.out.println("Enter the number of search queries:");
-        executeSearch(Integer.parseInt(scanner.nextLine()));
+        do {
+            setMenu();
+            try {
+                switch (menu) {
+                    case SEARCH -> executeSearch();
+                    case PRINT -> {
+                        System.out.println("=== List of people ===");
+                        peopleList.forEach(System.out::println);
+                    }
+                    case EXIT -> System.out.println("\nBye!");
+                }
+            } catch(NullPointerException e) {
+                System.out.println("Incorrect option! Try again");
+            }
+        } while (menu != Menu.EXIT);
     }
 }
